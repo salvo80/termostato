bb39177d4ec5442edcefe627ca421a1f48870f4f
salvolupo.g@gmail.com <salvolupo.g@gmail.com>
2019-11-06 00:17:41 +0100

    fix

--- application.lua
+++ application.lua
@@ -1,31 +1,61 @@
-temperature=""
+temperature=25
 STEP = .5
-PIN_PLUS = 3
+PIN_PLUS = 2
 PIN_MINUS = 4
+
+dofile("display.lua")
+
+if file.open("conf.lua") ~= nil then
+	dofile("conf.lua")
+end
+
+-- listening mqtt (temperature='t', wifi credentials='w')
+function eval_mqtt_w (client)
+	print(client)
+	if client ~= "iot" then
+		if topic == "w" then
+			local cre = file.open("credentials.lua","w")
+			cre.write(message)
+			cre.close()
+		end
+	end
+end
+function eval_mqtt_t (client)
+	print(client)
+	if client ~= "iot" then
+		if topic == "t" then
+			temperature = message
+		end
+	end
+end
+function onConnect(client) 
+	print("connected") 
+	--m:on(event, evalMQTT)
+	m:subscribe("w",0, eval_mqtt_w)
+	m:subscribe("t",0, eval_mqtt_t)
+end
+m = mqtt.Client("iot", 120, "fhnmelxv", "GVyrRJ7jSVxg")
+m:connect("farmer.cloudmqtt.com", 15778, false, onConnect, function(client, reason)print(reason)end)
+
+function updateConfig()
+	local conf = file.open("conf.lua","w")
+	conf.write("temperature="..temperature)
+	conf.close()
+end
 
 function onTempChange()
 	doRefreshDisplay()
 	updateConfig()
+	m:publish("t", temperature, 0, 0)
 end
--- listening mqtt (temperature, wifi credentials)
-function evalMQTT (client, topic, message)
-	if topic=="t" then
-		temperature=message
-	end
-	if topic=="w" then
-		local cre = file.open("credentials.lua","w")
-		cre.write(message)
-		cre.close()
-	end
-end
-mqtt:on(event, evalMQTT)
 
 -- listening pins
 -- Establish or clear a callback function to run on interrupt for a pin.
 -- This function is not available if GPIO_INTERRUPT_ENABLE was undefined at compile time.
 -- gpio.trig(pin, [type [, callback_function]])
-gpio.mode(PIN_PLUS,gpio.INT,gpio.PULLUP)
-gpio.mode(PIN_MINUS,gpio.INT,gpio.PULLUP)
+--gpio.mode(PIN_PLUS,gpio.INT)
+gpio.mode(PIN_PLUS,gpio.INPUT)
+--gpio.mode(PIN_MINUS,gpio.INT)
 function triggerPIN_PLUS(level, when, eventcount)
      triggerPIN(level, when, eventcount, STEP)
 end
@@ -34,11 +64,18 @@ function triggerPIN_MINUS(level, when, eventcount)
 end
 function triggerPIN(level, when, eventcount, value)
      temperature = temperature + value
+	 onTempChange()
 end
-gpio.trig(PIN_PLUS, "down", triggerPIN_PLUS)
-gpio.trig(PIN_MINUS, "down", triggerPIN_MINUS)
-
--- refreshing display
-function doRefreshDisplay()
-	
+--gpio.trig(PIN_PLUS, "high", triggerPIN_PLUS)
+--gpio.trig(PIN_MINUS, "high", triggerPIN_MINUS)
+loop_tmr = tmr.create()
+function loop()
+	if gpio.read(PIN_PLUS) == gpio.HIGH then
+		print("triggered!!")
+		triggerPIN_PLUS(0,0,0)
+		loop_tmr:stop()
+		tmr.create():alarm(900, tmr.ALARM_SINGLE, function() loop_tmr:start() end)
+	end
 end
+loop_tmr:register(100, tmr.ALARM_AUTO, loop) --ALARM_SEMI
+loop_tmr:start()
---
+++ display.lua
@@ -0,0 +1,34 @@
+DISPLAY_sda = 4 -- GPIO2 D4
+DISPLAY_scl = 3 -- GPIO0 D3
+
+function init_i2c_display()
+    local sla = 0x3c
+    i2c.setup(0, DISPLAY_sda, DISPLAY_scl, i2c.SLOW)
+    disp = u8g2.ssd1306_i2c_128x64_noname(0, sla)
+end
+
+init_i2c_display()
+
+function u8g2_prepare()
+  disp:setFont(u8g2.font_6x10_tf)
+  disp:setFontRefHeightExtendedText()
+  disp:setDrawColor(1)
+  disp:setFontPosTop()
+  disp:setFontDirection(0)
+end
+
+function draw()
+  u8g2_prepare()
+  
+  disp:drawStr( 0, 0, "Scegli la temperatura")
+  disp:setFont(u8g2.font_logisoso16_tr)
+  disp:drawStr( 10, 40, tostring(temperature).."°" )
+  
+end
+
+function doRefreshDisplay()
+  disp:clearBuffer()
+  draw()
+  disp:sendBuffer()
+end
+
